4cafd31938ac089d7c72957d614e456d73e94ea5
Diah Siti Fatimah A <63103085+diahazzahrah@users.noreply.github.com>
2022-03-24 20:54:21 +0700

    Create luas_lingkaran.py

---
+++ luas_lingkaran.py
@@ -0,0 +1,6 @@
+print('Menghitung luas bangun datar lingakaran')
+print()
+r = int(input('Masukkan ukuran jari-jari (r) lingkaran: '))
+phi = 3.14
+L = phi * (r * r)
+print('Luas lingakaran dengan jari-jari {} adalah {}'.format(r, L))
